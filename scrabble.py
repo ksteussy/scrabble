0acc239dabaa84688b0ca15af1f256acb3e6d4bb
ksteussy <ksteuss@gmail.com>
2020-03-31 14:39:13 -0500

    commit test

--- scrabble.py
+++ scrabble.py
@@ -2,15 +2,14 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
            "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"],
+                   "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+player_to_points = {}
+
 letters_to_points = {letter: point for letter, point in zip(letters, points)}
-# print(letters_to_points)
 
 letters_to_points[" "] = 0
 
-
-# print(letters_to_points)
-
-# print(letters_to_points["Z"])
 
 def score_word(word):
     score = 0
@@ -19,32 +18,24 @@ def score_word(word):
     return score
 
 
-# print(score_word("Hound"))
-
-player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"],
-                   "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
-player_to_points = {}
-
-
-# print(player_to_words)
-
-
-# print(player_to_points)
-
 def play_word(player, word):
     player_to_words[player] += [word]
 
 
+def update_point_totals():
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word)
+        player_to_points[player] = player_points
+
+
 play_word("player1", "fish")
-# print(player_to_words)
+play_word("wordNerd", "Vast")
+play_word("Lexi Con", "Frack")
+play_word("Prof Reader", "JUMBO")
 
-
-for player, words in player_to_words.items():
-    player_points = 0
-    for word in words:
-        player_points += score_word(word)
-    player_to_points[player] = player_points
+update_point_totals()
 
 print(player_to_points)
 
-#commit test2
